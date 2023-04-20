12e00344fefa45df1eb33f5392d3f0c0b03ad14a
Vexentrios <kagenokemonoaku@gmail.com>
2023-04-20 16:42:53 +0200

    Connection to Database and GitIgnore

--- GameScribe/settings.py
+++ GameScribe/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'GameScribe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '23Xt3uNhk1aNyJ5u4dLq',
+        'HOST': 'containers-us-west-190.railway.app',
+        'PORT': '7328',
     }
 }
 
